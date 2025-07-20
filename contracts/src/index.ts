43efe7d2d32dc89cf0725cfda28a33930947d2e8
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-07-20 10:25:07 +0100

    refactor repo to include UI only

--- contracts/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { FungibleToken } from './TokenBase.js';
-import { NoriStorageInterface } from './NoriStorageInterface.js';
-import { NoriTokenController } from './NoriTokenController.js';
-
-export { FungibleToken, NoriStorageInterface, NoriTokenController };
