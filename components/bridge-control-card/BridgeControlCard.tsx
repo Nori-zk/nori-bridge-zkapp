652de0be0e7e0cc4a6f5b6c9a49020854d9b6395
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-08-18 10:38:05 +0100

    refactor: pull SVG background into seperate file

--- components/bridge-control-card/BridgeControlCard.tsx
+++ components/bridge-control-card/BridgeControlCard.tsx
@@ -10,6 +10,7 @@ import ObtainCredentials from "@/components/bridge-control-card/ProgressSteps/Ob
 import LockTokens from "./ProgressSteps/LockTokens.tsx";
 import GetLockedTokens from "./ProgressSteps/GetLockedTokens.tsx";
 import { useZkappWorker } from "@/providers/ZkWorkerProvider/ZkWorkerProvider.tsx";
+import BridgeControlCardSVG from "./BridgeControlCardSVG.tsx";
 
 type BridgeControlCardProps = {
   title: string;
@@ -52,199 +53,47 @@ const BridgeControlCard = (props: BridgeControlCardProps) => {
         borderRadius: "20px",
       }}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="70 0 830 475"
-        width="830"
-        height="475"
-        style={{
-          // position: "absolute",
-          top: 0,
-          left: 0,
-          display: "block",
-        }}
-      >
-        <g filter="url(#filter0_d_273_5871)">
-          <rect
-            x="70"
-            width="830"
-            height="475"
-            rx="20"
-            fill="#060A08"
-            fillOpacity="0.1"
-            shapeRendering="crispEdges"
-          />
-          <rect
-            x="70"
-            width="830"
-            height="475"
-            rx="20"
-            fill="url(#paint0_radial_273_5871)"
-            fillOpacity="0.2"
-            shapeRendering="crispEdges"
-          />
-          <rect
-            x="70.5"
-            y="0.5"
-            width="829"
-            height="474"
-            rx="19.5"
-            stroke="url(#paint1_linear_273_5871)"
-            strokeOpacity="0.1"
-            shapeRendering="crispEdges"
-          />
-          <rect
-            x="70.5"
-            y="0.5"
-            width="829"
-            height="474"
-            rx="19.5"
-            stroke="url(#paint2_radial_273_5871)"
-            shapeRendering="crispEdges"
-          />
-          <rect
-            x="70.5"
-            y="0.5"
-            width="829"
-            height="474"
-            rx="19.5"
-            stroke="url(#paint3_radial_273_5871)"
-            shapeRendering="crispEdges"
-          />
-        </g>
-        <foreignObject x="70" y="0" width="830" height="475">
-          <div className="flex flex-col items-center justify-center h-full">
-            <h1 className="text-center text-white text-3xl mb-6">{title}</h1>
-            <div className="w-3/4">
-              <div className="flex text-white justify-between items-center">
-                <WalletButton
-                  id="eth-btn"
-                  types={"Ethereum"}
-                  content={
-                    ethConnected ? ethDisplayAddress ?? "" : "Connect Wallet"
-                  }
-                />
-                <div className="flex items-center justify-center w-7 h-7 text-black bg-white rounded-full mx-2">
-                  <FaArrowRight />
-                </div>
-                <WalletButton
-                  id="mina-btn"
-                  types={"Mina"}
-                  content={minaButtonContent}
-                />
+      <BridgeControlCardSVG width={props.width} height={props.height}>
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-center text-white text-3xl mb-6">{title}</h1>
+          <div className="w-3/4">
+            <div className="flex text-white justify-between items-center">
+              <WalletButton
+                id="eth-btn"
+                types={"Ethereum"}
+                content={
+                  ethConnected ? ethDisplayAddress ?? "" : "Connect Wallet"
+                }
+              />
+              <div className="flex items-center justify-center w-7 h-7 text-black bg-white rounded-full mx-2">
+                <FaArrowRight />
               </div>
-              <div className="flex justify-center mt-6 text-white">
-                {isWorkerLoading || !state.context.zkappWorkerClient ? (
-                  <p>Spinning up zkappWorker...</p>
-                ) : !zkappWorkerClient ? (
-                  <p>zkappWorker is not ready.</p>
-                ) : !compiledEcdsaCredential ? (
-                  <p>Running step compiledEcdsaCredential...</p>
-                ) : state.context.step === "create" ? (
-                  <CreateCredentials />
-                ) : state.context.step === "obtain" ? (
-                  <ObtainCredentials />
-                ) : state.context.step === "lock" ? (
-                  <LockTokens />
-                ) : (
-                  <GetLockedTokens />
-                )}
-              </div>
+              <WalletButton
+                id="mina-btn"
+                types={"Mina"}
+                content={minaButtonContent}
+              />
+            </div>
+            <div className="flex justify-center mt-6 text-white">
+              {isWorkerLoading || !state.context.zkappWorkerClient ? (
+                <p>Spinning up zkappWorker...</p>
+              ) : !zkappWorkerClient ? (
+                <p>zkappWorker is not ready.</p>
+              ) : !compiledEcdsaCredential ? (
+                <p>Running step compiledEcdsaCredential...</p>
+              ) : state.context.step === "create" ? (
+                <CreateCredentials />
+              ) : state.context.step === "obtain" ? (
+                <ObtainCredentials />
+              ) : state.context.step === "lock" ? (
+                <LockTokens />
+              ) : (
+                <GetLockedTokens />
+              )}
             </div>
           </div>
-        </foreignObject>
-        <defs>
-          <filter
-            id="filter0_d_273_5871"
-            x="0"
-            y="-20"
-            width="970"
-            height="635"
-            filterUnits="userSpaceOnUse"
-            colorInterpolationFilters="sRGB"
-          >
-            <feFlood floodOpacity="0" result="BackgroundImageFix" />
-            <feColorMatrix
-              in="SourceAlpha"
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              result="hardAlpha"
-            />
-            <feMorphology
-              radius="50"
-              operator="erode"
-              in="SourceAlpha"
-              result="effect1_dropShadow_273_5871"
-            />
-            <feOffset dx="-20" dy="70" dx="20" />
-            <feGaussianBlur stdDeviation="60" />
-            <feComposite in2="hardAlpha" operator="out" />
-            <feColorMatrix
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0"
-            />
-            <feBlend
-              mode="normal"
-              in2="BackgroundImageFix"
-              result="effect1_dropShadow_273_5871"
-            />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="effect1_dropShadow_273_5871"
-              result="shape"
-            />
-          </filter>
-          <radialGradient
-            id="paint0_radial_273_5871"
-            cx="0"
-            cy="0"
-            r="1"
-            gradientUnits="userSpaceOnUse"
-            gradientTransform="translate(70) rotate(33.5305) scale(995.691 588.873)"
-          >
-            <stop stopColor="#03FF9F" stopOpacity="0.2" />
-            <stop offset="0.2" stopColor="#03FF9F" stopOpacity="0.1" />
-            <stop offset="0.48476" stopColor="#03FF9F" stopOpacity="0" />
-          </radialGradient>
-          <linearGradient
-            id="paint1_linear_273_5871"
-            x1="485"
-            y1="0"
-            x2="485"
-            y2="475"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="white" />
-            <stop offset="1" stopColor="#999999" />
-          </linearGradient>
-          <radialGradient
-            id="paint2_radial_273_5871"
-            cx="0"
-            cy="0"
-            r="1"
-            gradientUnits="userSpaceOnUse"
-            gradientTransform="translate(70) rotate(90) scale(264.137 225.049)"
-          >
-            <stop stopColor="#64E18E" />
-            <stop offset="0.25" stopColor="#1F6344" />
-            <stop offset="1" stopColor="#1F6344" stopOpacity="0" />
-          </radialGradient>
-          <radialGradient
-            id="paint3_radial_273_5871"
-            cx="0"
-            cy="0"
-            r="1"
-            gradientUnits="userSpaceOnUse"
-            gradientTransform="translate(900) rotate(90) scale(254.99 244.107)"
-          >
-            <stop stopColor="#64E18E" />
-            <stop offset="0.25" stopColor="#1F6344" />
-            <stop offset="1" stopColor="#1F6344" stopOpacity="0" />
-          </radialGradient>
-        </defs>
-      </svg>
+        </div>
+      </BridgeControlCardSVG>
     </div>
   );
 };
