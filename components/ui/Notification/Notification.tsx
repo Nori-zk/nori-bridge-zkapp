34f6cbf184382cf0513376f7a82e68e540987357
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-08-20 10:20:03 +0100

    feat: connect wallet notification show/hide on page load/wallets connect

--- components/ui/Notification/Notification.tsx
+++ components/ui/Notification/Notification.tsx
@@ -1,12 +1,19 @@
 type NotificationProps = {
   content?: string;
   status?: "success" | "error" | "info"; // not yet implemented, for dynamic styling
+  show?: boolean;
 };
 
-const Notification = ({ content }: NotificationProps) => {
+const Notification = ({ content, show = false }: NotificationProps) => {
   return (
-    <div className="rounded-tr-xl rounded-tl-xl mb-3 px-8 fixed left-1/2 -translate-x-1/2 z-50 bg-green-900 ">
-      <div className="bottom-6 py-2 text-lightGreen">{content}</div>
+    <div
+      className={`rounded-tr-xl rounded-tl-xl mb-2 px-8 fixed left-1/2 -translate-x-1/2 z-50 bg-green-900 transition-all duration-500 ${
+        show
+          ? "translate-y-0 opacity-100"
+          : "translate-y-full opacity-0 pointer-events-none"
+      }`}
+    >
+      <div className="py-2 text-lightGreen">{content}</div>
     </div>
   );
 };
